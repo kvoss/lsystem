79215ff89b76650cece6edb6fe7e841e4e81a276
Krzysztof Voss <kmv633@mail.usask.ca>
2014-08-12 13:33:30 -0600

    install script

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+CONFIG = {
+    'name'             : 'lsystem',
+    'description'      : 'Implementation of L-Systems',
+    'version'          : '0.1.0',
+
+    'author'           : 'Krzysztof Voss',
+    'author_email'     : 'k.voss@usask.ca',
+
+    'license'          : 'BSD',
+    'platforms'        : ['POSIX',],
+    'keywords'         : 'LSystem, turtle, fractals, rewrite',
+    'py_modules'       : ['lsystem'],
+    'url'              : 'https://github.com/kvoss/lsystem',
+    'download_url'     : 'https://github.com/kvoss/lsystem',
+    'classifiers'      : [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7', ],
+    }
+
+
+setup(**CONFIG)
+
