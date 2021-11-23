6980022e5296121f1caf236ec2e8d2ece004e787
K. Voss <kvoss@users.noreply.github.com>
2021-11-22 19:03:05 -0600

    #1 update setup with Python3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 CONFIG = {
     'name'             : 'lsystems',
     'description'      : 'Implementation of L-Systems',
-    'version'          : '0.1.0',
+    'version'          : '0.1.1',
 
     'author'           : 'Krzysztof Voss',
     'author_email'     : 'k.voss@usask.ca',
@@ -18,7 +18,14 @@ CONFIG = {
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7', ],
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
     }
 
 
